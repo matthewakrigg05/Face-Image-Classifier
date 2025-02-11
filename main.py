2a5ab422dca23e3463491b63ad7c3a39241fed1b
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-11 20:38:46 +0000

    refactor: parts of model split into different files

---
+++ main.py
@@ -0,0 +1,12 @@
+import dataHandling as dh
+import model as model
+
+def main():
+    data = dh.load_images_from_directories()
+
+    x_train, x_test, y_train, y_test = dh.split_data(data)
+
+    model.svm_classifier(x_train, x_test, y_train, y_test)
+
+if __name__ == '__main__':
+    main()
